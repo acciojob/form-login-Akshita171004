8f1107b7114fa040e80fa935610adb2bd4c35c6a
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-18 00:48:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from reloading the page
+    event.preventDefault();function getFormvalue(event) {
+  event.preventDefault(); // Prevent page reload
 
+  const form = document.getElementById("form1");
+  const fname = form.fname.value.trim();
+  const lname = form.lname.value.trim();
+
+  alert(fname + " " + lname);
+}
+
+
+    // Get the form element
+    const form = document.forms[0];
+
+    // Get and trim the values of the input fields
+    const fname = form.fname.value.trim();
+    const lname = form.lname.value.trim();
+
+    // Show the full name in an alert
+    alert(fname + " " + lname);
 }
